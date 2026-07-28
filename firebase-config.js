8c2782a5f6a485d6a7aea362e82f5b8e4ae3464c
ViewEC-Sandy <sandywangfdmkt@gmail.com>
2026-07-28 09:53:07 +0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // 請將 Firebase Console > 專案設定 > 您的應用程式 > SDK 設定內容貼到這裡。
 export const firebaseConfig = {
-  apiKey: "AIzaSyCji9JRzfnR_E488uw_Kl_i3XryN6aZ5OQ",
-  authDomain: "japan-ec-dashboard.firebaseapp.com",
-  projectId: "japan-ec-dashboard",
-  storageBucket: "japan-ec-dashboard.firebasestorage.app",
-  messagingSenderId: "365666288337",
-  appId: "1:365666288337:web:382d58336add6ce9e23bf0"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
